d0fe847b7cdfdad3fec7ab58020a823106378f22
Hirotaka Osaki <hioosaki@gmail.com>
2015-06-07 17:44:25 +0900

    refactoring

--- schema/util.go
+++ schema/util.go
@@ -1,6 +1,16 @@
 package schema
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/ghodss/yaml"
+)
 
 func String(target interface{}, key string) string {
 	d, ok := target.(map[string]interface{})
@@ -47,4 +57,90 @@ func Interface(target interface{}, key string, typeStr string) interface{} {
 		return String(target, key)
 	}
 	return v
+}
+
+func isSupportExt(s string) bool {
+	return isJSONExt(s) || isYAMLExt(s)
+}
+
+func isJSONExt(s string) bool {
+	ext := path.Ext(s)
+	return ext == ".json"
+}
+
+func isYAMLExt(s string) bool {
+	ext := path.Ext(s)
+	return ext == ".yaml" || ext == ".yml"
+}
+
+func baseResourceName(s string) string {
+	ss := strings.Split(s, "/")
+	name := ss[len(ss)-1]
+
+	ext := path.Ext(name)
+	return name[0 : len(name)-len(ext)]
+}
+
+func isExtJSONFile(info os.FileInfo) bool {
+	return isFile(info) && isMatchExt(info, ".json")
+}
+
+func isExtYaml(info os.FileInfo) bool {
+	return isFile(info) && isMatchExt(info, ".yml", ".yaml")
+}
+
+func isMatchExt(info os.FileInfo, exts ...string) bool {
+	e := filepath.Ext(info.Name())
+	for _, ext := range exts {
+		if e == ext {
+			return true
+		}
+	}
+	return false
+}
+
+func isFile(info os.FileInfo) bool {
+	if info == nil {
+		return false
+	}
+	if info.IsDir() {
+		return false
+	}
+	return true
+}
+
+func resourceName(name string) string {
+	base := path.Base(name)
+	e := filepath.Ext(base)
+	return base[0 : len(base)-len(e)]
+}
+
+func YamlFileToJson(path string, info os.FileInfo) ([]byte, error) {
+	isJSON := isExtJSONFile(info)
+	isYAML := isExtYaml(info)
+
+	if !isJSON && !isYAML {
+		return nil, fmt.Errorf("%v is not support file format", info.Name())
+	}
+
+	rs, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var d map[string]interface{}
+	switch {
+	case isJSON:
+		if err := json.Unmarshal(rs, &d); err != nil {
+			return nil, err
+		}
+	case isYAML:
+		if err := yaml.Unmarshal(rs, &d); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("%v is not support file format", info.Name())
+	}
+
+	return json.MarshalIndent(d, "", "  ")
 }
