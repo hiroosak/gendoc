c72c19d36b1066e17de2fe699978c1637419d2cd
Hirotaka Osaki <hioosaki@gmail.com>
2015-06-07 10:54:21 +0900

    refactoring schema.go

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -2,183 +2,178 @@ package schema
 
 import (
 	"encoding/json"
-	"reflect"
 	"testing"
 )
 
-func readTestData(t *testing.T) (*Schema, *Schema) {
-	user, err := newTestSchema(userJSON)
+func TestExampleJSON(t *testing.T) {
+	s, err := NewSchemaFromBytes([]byte(userJSON), "", nil)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("invalid json")
 	}
-	article, err := newTestSchema(articleJSON)
-	if err != nil {
-		t.Fatal(err)
-	}
-	return user, article
-}
+	jsonStr := s.ExampleJSON()
 
-func TestNewSchema(t *testing.T) {
-	user, article := readTestData(t)
-
-	s := article.ExampleJSON()
 	var i map[string]interface{}
-	if err := json.Unmarshal([]byte(s), &i); err != nil {
-		t.Fatal("invalid article json")
+	if err := json.Unmarshal([]byte(jsonStr), &i); err != nil {
+		t.Fatalf("invalid example json. %v", err)
 	}
-	articleUser, ok := i["user"].(map[string]interface{})
-	if !ok {
-		t.Fatal("invalid article json")
-
+	if i["age"].(float64) != 16 {
+		t.Errorf("age is expected 16. but %v", i["age"])
 	}
-	if !reflect.DeepEqual(articleUser, user.ExampleInterface()) {
-		t.Errorf("article.user %v wants to equal user %v", articleUser, user.ExampleInterface())
+	if i["id"].(string) != "1" {
+		t.Errorf("id is expected 1. but %v", i["id"])
+	}
+	if i["name"].(string) != "Mike" {
+		t.Errorf("name is expected Mike. but %v", i["name"])
 	}
 }
 
-func TestExampleGetData(t *testing.T) {
-	_, article := readTestData(t)
+var t1 *Schema = &Schema{
+	Id: "user",
+	Definitions: map[string]*Schema{
+		"username": &Schema{
+			Description: "user's name",
+			Type:        "string",
+			Example:     "Ken",
+		},
+		"age": &Schema{
+			Description: "user's old",
+			Type:        "integer",
+			Example:     18,
+		},
+	},
+	Type: "array",
+	Items: []*Schema{
+		&Schema{
+			Ref: "#",
+		},
+	},
+}
 
-	for _, l := range article.Links {
-		if l.Rel == "instances" && l.Href == "/articles" {
-			getdata := l.Schema.ExampleGetData()
-			if getdata[0] != "name=Article" {
-				t.Errorf("expected name=Article, get %v", getdata[0])
+func TestExampleJSONArray(t *testing.T) {
+	var jsonstr = `{
+		"id": "user",
+		"definitions": {
+			"age": {
+				"example": 16,
+				"description": "user's age",
+				"type": "integer"
+			},
+			"id": {
+				"example": 1,
+				"type": "string"
 			}
-			return
+		},
+		"type": "array",
+		"items": {
+			"$ref": "#/definitions/age"
 		}
+	}`
+
+	t1, err := NewSchemaFromBytes([]byte(jsonstr), "", nil)
+	if err != nil {
+		t.Fatal(err)
 	}
-	t.Errorf("instances link is not found")
+
+	j := t1.ExampleJSON()
+	var jm []interface{}
+	json.Unmarshal([]byte(j), &jm)
+
+	if len(jm) != 1 {
+		t.Fatalf("len(j) length is not 1. %v", len(jm))
+	}
+	if jm[0].(float64) != 16 {
+		t.Errorf("age is not 16. %v", j)
+	}
 }
 
-func newTestSchema(str string) (*Schema, error) {
-	var data map[string]interface{}
-	if err := json.Unmarshal([]byte(str), &data); err != nil {
-		return nil, err
-	}
-	return NewSchema(data, nil)
-}
-
-const articleJSON = `{
-  "$schema": "http://json-schema.org/draft-04/hyper-schema",
-  "definitions": {
-    "created_at": {
-      "description": "when articles was created",
-      "example": "2012-01-01T12:00:00Z",
-      "format": "date-time",
-      "type": [
-        "string"
-      ]
-    },
-    "id": {
-      "description": "unique identifier of articles",
-      "example": "01234567-89ab-cdef-0123-456789abcdef",
-      "format": "uuid",
-      "type": [
-        "string"
-      ]
-    },
-    "updated_at": {
-      "description": "when articles was updated",
-      "example": "2012-01-02T12:00:00Z",
-      "format": "date-time",
-      "type": [
-        "string"
-      ]
-    }
-  },
-  "description": "FIXME",
-  "id": "articles",
-  "links": [
-    {
-      "description": "Create a new articles.",
-      "href": "/articles",
-      "method": "POST",
-      "rel": "create",
-      "schema": {
-        "properties": {
-          "id": {
-            "$ref": "#/definitions/id"
-          }
-        },
-        "type": [
-          "object"
-        ]
-      },
-      "title": "Create"
-    },
-    {
-      "description": "Delete an existing articles.",
-      "href": "/articles/{id}",
-      "method": "DELETE",
-      "rel": "destroy",
-      "title": "Delete"
-    },
-    {
-      "description": "Info for existing articles.",
-      "href": "/articles/{id}",
-      "method": "GET",
-      "rel": "self",
-      "title": "Info"
-    },
-    {
-      "description": "List existing articles.",
-      "href": "/articles",
-      "method": "GET",
-      "rel": "instances",
-      "title": "List",
-			"encType": "aplication/x-www-form-urlencoded",
-			"schema": {
+func TestExampleJSONArrayObject(t *testing.T) {
+	var jsonstr = `{
+		"id": "user",
+		"definitions": {
+			"data": {
 				"type": "object",
 				"properties": {
-					"name": {
-						"descritption": "name of the product",
-						"example": "Article"
+					"age": {
+						"example": 16,
+						"description": "user's age",
+						"type": "integer"
 					}
 				}
 			}
-    },
-    {
-      "description": "Update an existing articles.",
-      "href": "/articles/{id}",
-      "method": "PATCH",
-      "rel": "update",
-      "schema": {
-        "properties": {},
-        "type": [
-          "object"
-        ]
-      },
-      "title": "Update"
-    }
-  ],
-  "properties": {
-    "created_at": {
-      "$ref": "#/definitions/created_at"
-    },
-    "id": {
-      "$ref": "#/definitions/id"
-    },
-    "updated_at": {
-      "$ref": "#/definitions/updated_at"
-    },
-    "user": {
-      "$ref": "user.json"
-    }
-  },
-  "title": "FIXME - Articles",
-  "type": [
-    "object"
-  ]
+		},
+		"type": "array",
+		"items": {
+			"$ref": "#/definitions/data"
+		}
+	}`
+
+	t1, err := NewSchemaFromBytes([]byte(jsonstr), "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	j := t1.ExampleJSON()
+	var jm []map[string]interface{}
+	err = json.Unmarshal([]byte(j), &jm)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(jm) != 1 {
+		t.Fatalf("len(j) length is not 1. %v", len(jm))
+	}
+	if jm[0]["age"].(float64) != 16 {
+		t.Errorf("age is not 16. %v", j)
+	}
 }
-`
+
+func TestRefpool(t *testing.T) {
+	_, err := NewSchemaFromBytes([]byte(userJSON), "", nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestResolveReference(t *testing.T) {
+	s, err := NewSchemaFromBytes([]byte(userJSON), "", nil)
+	if err != nil {
+		t.Fatal("invalid json")
+	}
+	r1 := s.resolveReference("user", "#/definitions/age")
+	if r1.Description != "user's age" {
+		t.Errorf("description is not equal")
+	}
+	if r1.Example != float64(16) {
+		t.Errorf("example is not equal %v != %v", r1.Example, 16)
+	}
+}
+
+func TestAppendRefPath(t *testing.T) {
+	type t2 struct {
+		src []string
+		dst string
+	}
+
+	res := []t2{
+		t2{[]string{"a"}, "#/a"},
+		t2{[]string{"definitions", "id"}, "#/definitions/id"},
+	}
+
+	s := &Schema{CurrentRef: "#"}
+	for _, r := range res {
+		if s.appendRefPath(r.src...) != r.dst {
+			t.Errorf("expected %v. but actual %v", r.src, r.dst)
+		}
+	}
+}
 
 const userJSON = `{
   "$schema": "http://json-schema.org/draft-04/hyper-schema",
   "definitions": {
     "age": {
       "example": 16,
-      "type": "integer"
+			"description": "user's age",
+			"type": "integer"
     },
     "id": {
       "example": 1,
--- schema/util.go
+++ schema/util.go
@@ -1,12 +1,6 @@
 package schema
 
-import (
-	"fmt"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
-)
+import "fmt"
 
 func String(target interface{}, key string) string {
 	d, ok := target.(map[string]interface{})
@@ -53,60 +47,4 @@ func Interface(target interface{}, key string, typeStr string) interface{} {
 		return String(target, key)
 	}
 	return v
-}
-
-func isSupportExt(s string) bool {
-	return isJSONExt(s) || isYAMLExt(s)
-}
-
-func isJSONExt(s string) bool {
-	ext := path.Ext(s)
-	return ext == ".json"
-}
-
-func isYAMLExt(s string) bool {
-	ext := path.Ext(s)
-	return ext == ".yaml" || ext == ".yml"
-}
-
-func baseResourceName(s string) string {
-	ss := strings.Split(s, "/")
-	name := ss[len(ss)-1]
-
-	ext := path.Ext(name)
-	return name[0 : len(name)-len(ext)]
-}
-
-func isExtJSONFile(info os.FileInfo) bool {
-	return isFile(info) && isMatchExt(info, ".json")
-}
-
-func isExtYaml(info os.FileInfo) bool {
-	return isFile(info) && isMatchExt(info, ".yml", ".yaml")
-}
-
-func isMatchExt(info os.FileInfo, exts ...string) bool {
-	e := filepath.Ext(info.Name())
-	for _, ext := range exts {
-		if e == ext {
-			return true
-		}
-	}
-	return false
-}
-
-func isFile(info os.FileInfo) bool {
-	if info == nil {
-		return false
-	}
-	if info.IsDir() {
-		return false
-	}
-	return true
-}
-
-func resourceName(name string) string {
-	base := path.Base(name)
-	e := filepath.Ext(base)
-	return base[0 : len(base)-len(e)]
 }
